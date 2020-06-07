551e490cc32e9e7364b8d806eeee5d6f304c0e5a
bhsd <2545473905@qq.com>
2020-06-07 12:11:32 -0400

    More regex

--- js/script.js
+++ js/script.js
@@ -353,7 +353,7 @@ function upload(){
     combo=[];
     let lines=$$("txtTable").split('\n');
     for(let k=0;k<lines.length;k++){
-	let item=lines[k].replace(/^(第|属|阅|本|折).*/g,'');
+	let item=lines[k].replace(/^(活|第|属|阅|本|折).*/g,'');
 	if(item==''){continue;}
 	let tabs=item.split('\t');
 	let song_comment=tabs[getInt("txtCol")-1].trim();
